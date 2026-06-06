b1ef4d9380e4ae87b5e12b73acd02a919ac5489e
25023558-blip <25023558@vnu.edu.vn>
2026-06-06 15:37:41 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Hiệu ứng gõ chữ (Typed.js)
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Web Designer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
